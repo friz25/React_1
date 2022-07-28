959442ed900e1e09313a7f7b9ef339dcc618a50c
frizmob@gmail.com <friz48625>
2022-07-28 21:55:38 +0300

    shop v1.8 добавление товара (работает)

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ import {ErrorMessage} from "./components/ErrorMessage";
 import {Modal} from "./components/Modal";
 import {CreateProduct} from "./components/CreateProduct";
 import {useState} from "react";
+import {IProduct} from "./models";
 
 
 function App() {
-    const { loading, error, products } = useProducts()
+    const { loading, error, products, addProduct } = useProducts()
     const [modal, setModal] = useState( true)
+
+    const createHandler = (product: IProduct) => {
+        setModal(false)
+        addProduct(product)
+    }
 
     return (
         <div className="container mx-auto max-w-2xl pt-5">
@@ -20,7 +26,7 @@ function App() {
             { products.map(product => <Product product={product} key={product.id}/>) }
 
             {modal && <Modal title="Create new product">
-                <CreateProduct onCreate={() => setModal(false)} />
+                <CreateProduct onCreate={createHandler} />
             </Modal>}
         </div>
     )
