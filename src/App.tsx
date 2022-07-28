9e5462f3942d04bcbafa7ca9abb79759ea69e27b
frizmob@gmail.com <friz48625>
2022-07-28 19:58:07 +0300

    shop v1.3.1 try {} catch {}

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React, {createElement as e, useEffect, useState} from 'react';
 import {Product} from './components/Product'
 // import {products} from "./data/products";
-import axios from "axios";
+import axios, {AxiosError} from "axios";
 import {IProduct} from "./models";
 
 
 function App() {
     const [products, setProducts] = useState<IProduct[]>([])
     const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
 
     async function fetchProducts() {
-        setLoading(true)
-        const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-        setProducts(response.data)
-        setLoading(false)
+        try {
+            setError('')
+            setLoading(true)
+            const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+            setProducts(response.data)
+            setLoading(false)
+        } catch (e: unknown) {
+            const error = e as AxiosError
+            setLoading(false)
+            setError(error.message)
+        }
+        
+
     }
 
     useEffect( () => {
@@ -23,6 +33,8 @@ function App() {
     return (
         <div className="container mx-auto max-w-2xl pt-5">
             {loading && <p className="text-center">Loading...</p>}
+            {error && <p className="text-center text-red-600">{ error }</p>}
+
             {/*-----ЦЫКЛ------*/}
             { products.map(product => <Product product={product} key={product.id}/>) }
         </div>
