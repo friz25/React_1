62c484eeaf2fd143f1bdedd84cbf5e230ddbeb6a
frizmob@gmail.com <friz48625>
2022-07-28 22:15:39 +0300

    shop v1.9 смену атрибутов (преобразовали) в читабельные open(),close()

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import {Loader} from "./components/Loader";
 import {ErrorMessage} from "./components/ErrorMessage";
 import {Modal} from "./components/Modal";
 import {CreateProduct} from "./components/CreateProduct";
-import {useState} from "react";
+import {useContext} from "react";
 import {IProduct} from "./models";
+import {ModalContext} from "./context/ModalContext";
 
 
 function App() {
     const { loading, error, products, addProduct } = useProducts()
-    const [modal, setModal] = useState( false)
+    const { modal, open, close } = useContext(ModalContext)
 
     const createHandler = (product: IProduct) => {
-        setModal(false)
+        close()
         addProduct(product)
     }
 
@@ -25,13 +26,13 @@ function App() {
             {/*-----ЦЫКЛ------*/}
             { products.map(product => <Product product={product} key={product.id}/>) }
 
-            {modal && <Modal title="Create new product" onClose={() => setModal(false)}>
+            {modal && <Modal title="Create new product" onClose={close}>
                 <CreateProduct onCreate={createHandler} />
             </Modal>}
 
             <button
                 className="fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"
-                onClick={() => setModal(true)}
+                onClick={open}
             >+</button>
         </div>
     )
